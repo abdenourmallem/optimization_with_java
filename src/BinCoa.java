7ec788384bab8295bef00938a3e5495cbd195feb
Islam Idder <laloumdude@gmail.com>
2025-04-21 11:07:15 +0100

    update .gitignore

--- src/BinCoa.java
+++ src/BinCoa.java
@@ -1,10 +1,10 @@
 import java.util.Arrays;
 
-import tools.*;
+import tools.MKP;
 
 public class BinCoa {
     public static void main(String[] args) {
-        MKP mkpInstance = new MKP("..\\All-MKP-Instances\\chubeas\\OR5x100\\OR5x100-0.25_1.dat");
+        MKP mkpInstance = new MKP("..\\All-MKP-Instances\\chubeas\\OR5x100\\OR5x100-0.50_1.dat");
         System.out.println(mkpInstance.details[0]);
         System.out.println(mkpInstance.details[1]);
         System.out.println(Arrays.toString(mkpInstance.profits));
@@ -14,5 +14,6 @@ public class BinCoa {
         }
 
         System.out.println(Arrays.toString(mkpInstance.capacities));
+        System.out.println("added new thing");
     }
 }
